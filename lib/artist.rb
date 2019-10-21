0942f2702a33bf57b811b5950d647950b9713dc4
Janay Bailey <tjanay87@gmail.com>
2019-10-21 20:20:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,46 +3,25 @@ require_relative '../lib/concerns/memorable'
 require 'pry'
 
 class Artist
-  extend Memorable
-  attr_accessor :name
-  attr_reader :songs
-
+ 
   @@artists = []
-
-  def initialize
-    @@artists << self
-    @songs = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
   end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
+ 
   def self.all
     @@artists
   end
-
- #def self.reset_all
-  # self.all.clear
-# end
-
- #def self.count
-  # self.all.count
-# end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
+ 
+  def self.find_by_name(name)
+    @@artists.detect {|a| a.name == name}
   end
-
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
+end
 
 module Paramable
 end
 
- #def to_param
-  # name.downcase.gsub(' ', '-')
-# end
 end
