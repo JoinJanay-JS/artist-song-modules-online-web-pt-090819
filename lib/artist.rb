c63aa89b3e0333a8ad283fa0cbdeea1b498b7523
Janay Bailey <tjanay87@gmail.com>
2019-10-21 17:55:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -36,7 +36,9 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+module Paramable
+end
+ #def to_param
+  # name.downcase.gsub(' ', '-')
+# end
 end
